37df7c7b4e084ee6db882edfb123c46350ac3d98
Gustavo Maciel <gustavohcmaciel15@gmail.com>
2025-09-11 18:44:41 -0300

    inicio dos templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
-from django.shortcuts import render  # Adicionado - Mostrar templates
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Gustavo',
     })
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
