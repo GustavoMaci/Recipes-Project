cc0f531dce81e22ce6c289b03789226a5aee8a61
Gustavo Maciel <gustavohcmaciel15@gmail.com>
2025-09-13 07:01:02 -0300

    Novo template de receita

--- recipes/views.py
+++ recipes/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Gustavo',
+        'name': 'Gustavo Maciel',
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Gustavo Maciel',
     })
